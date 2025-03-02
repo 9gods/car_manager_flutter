474765580737b1623f342f09b2a4a026125cf16e
9gods <codingcomelli@gmail.com>
2025-03-01 22:45:50 -0300

    feat: implements car entity

---
+++ lib/domain/entities/car.dart
@@ -0,0 +1,25 @@
+class Car {
+  int id;
+  String brand;
+  String model;
+  String engine;
+  String engineCode;
+  int year;
+  int horsepower;
+  int price;
+  int weight;
+  // File carPhoto;
+
+  Car({
+    required this.id,
+    required this.brand,
+    required this.model,
+    required this.engine,
+    required this.engineCode,
+    required this.year,
+    required this.horsepower,
+    required this.price,
+    required this.weight,
+    // required File carPhoto,
+  });
+}

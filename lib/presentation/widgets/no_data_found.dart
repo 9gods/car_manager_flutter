cf75500dab3a2ae3e7be55eba8d06b9d4cf9deec
9gods <codingcomelli@gmail.com>
2025-03-02 05:16:31 -0300

    feat: implements no data found custom widget

---
+++ lib/presentation/widgets/no_data_found.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
+import 'package:garage_manager_flutter/presentation/widgets/colors/app_colors.dart';
+import 'package:garage_manager_flutter/presentation/widgets/primitives/general_card.dart';
+
+class NoDataFound extends StatelessWidget {
+  final String description;
+  const NoDataFound({required Key? key, required this.description})
+    : super(key: key);
+
+  Widget build(BuildContext context) {
+    return CustomCard(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(LucideIcons.circle_help, size: 20, color: Colors.red.shade400),
+            Text(
+              description,
+              style: TextStyle(
+                color: AppColors.slate300,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

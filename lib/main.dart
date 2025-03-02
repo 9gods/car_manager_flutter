889dddbcc4fd95a262e3b793a98fc127cdc3ee10
9gods <codingcomelli@gmail.com>
2025-03-02 06:27:52 -0300

    refactor: implements all routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:garage_manager_flutter/presentation/pages/about_this_app_page.dart';
+import 'package:garage_manager_flutter/presentation/pages/car_detail_page.dart';
+import 'package:garage_manager_flutter/presentation/pages/car_form_page.dart';
+import 'package:garage_manager_flutter/presentation/pages/cars_display_page.dart';
+import 'package:garage_manager_flutter/presentation/pages/home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Garage Manager',
       theme: ThemeData(textTheme: GoogleFonts.montserratTextTheme()),
-      home: const Scaffold(body: Center(child: Text('Hello World!'))),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/about': (context) => const AboutThisAppPage(),
+        '/car-detail': (context) => const CarDetailPage(),
+        '/car-form': (context) => const CarFormPage(),
+        '/cars-display': (context) => const CarsDisplayPage(),
+      },
     );
   }
 }

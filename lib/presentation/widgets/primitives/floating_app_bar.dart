f8d0a876889267644c7345d6ea35830e3c17a496
9gods <codingcomelli@gmail.com>
2025-03-02 06:14:31 -0300

    feat: implements new features on floating bar

--- lib/presentation/widgets/primitives/floating_app_bar.dart
+++ lib/presentation/widgets/primitives/floating_app_bar.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
 import 'package:garage_manager_flutter/presentation/widgets/colors/app_colors.dart';
+import 'package:garage_manager_flutter/presentation/widgets/primitives/custom_logo.dart';
+import 'package:garage_manager_flutter/presentation/widgets/primitives/dropdown_menu.dart';
 
 class FloatingAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final bool isHome;
   final String title;
-  final List<Widget>? actions;
 
-  const FloatingAppBar({Key? key, required this.title, this.actions})
+  const FloatingAppBar({Key? key, required this.isHome, this.title = ''})
     : super(key: key);
 
   @override
   Size get preferredSize => const Size.fromHeight(60);
+
+  void _showDropdown(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => const CustomDropdownMenu(),
+      backgroundColor: Colors.transparent,
+      isScrollControlled: true,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,11 +39,23 @@ class FloatingAppBar extends StatelessWidget implements PreferredSizeWidget {
             ),
           ],
         ),
-        child: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          title: Text(title, style: const TextStyle(color: AppColors.slate300)),
-          actions: actions,
+        child: Padding(
+          padding: EdgeInsets.all(12),
+          child: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            title: CustomLogo(isHome: isHome, title: title),
+            actions: [
+              IconButton(
+                icon: const Icon(
+                  LucideIcons.sandwich,
+                  color: AppColors.slate300,
+                ),
+                onPressed: () => _showDropdown(context),
+              ),
+            ],
+          ),
         ),
       ),
     );

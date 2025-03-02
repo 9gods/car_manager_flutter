852c7c8e961d3e3490c1679fb1013a781f1e2fb8
9gods <codingcomelli@gmail.com>
2025-03-01 23:11:52 -0300

    feat: implements create car use case

---
+++ lib/domain/use-cases/create_car.dart
@@ -0,0 +1,28 @@
+import 'package:car_manager_flutter/domain/repositories/car_repository.dart';
+
+class CreateCar {
+  final CarRepository repository;
+  CreateCar(this.repository);
+
+  Future<void> call(
+    String brand,
+    String model,
+    String engine,
+    String engineCode,
+    int year,
+    int horsepower,
+    int price,
+    int weight,
+  ) async {
+    return await repository.createCar(
+      brand,
+      model,
+      engine,
+      engineCode,
+      year,
+      horsepower,
+      price,
+      weight,
+    );
+  }
+}

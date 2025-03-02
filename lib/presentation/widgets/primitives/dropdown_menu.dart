476492d9c24bd3b0512a2d1d725a9433d10ea35d
9gods <codingcomelli@gmail.com>
2025-03-02 05:17:11 -0300

    feat: implements dropdown menu widget

---
+++ lib/presentation/widgets/primitives/dropdown_menu.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
+import 'package:garage_manager_flutter/presentation/widgets/colors/app_colors.dart';
+import 'package:garage_manager_flutter/presentation/widgets/primitives/dropdown_menu_button_item.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class DropdownMenu extends StatelessWidget {
+  const DropdownMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: AppColors.slate800,
+        border: Border.all(color: AppColors.slate700),
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          DropdownMenuButtonItem(
+            icon: LucideIcons.info,
+            text: 'About this app',
+            onPressed: () {
+              Navigator.pushNamed(context, '/about');
+            },
+          ),
+          DropdownMenuButtonItem(
+            icon: LucideIcons.external_link,
+            text: 'About Flutter',
+            onPressed: () async {
+              final url = Uri.parse('https://flutter.dev/');
+              if (await canLaunchUrl(url)) {
+                await launchUrl(url);
+              }
+            },
+          ),
+          DropdownMenuButtonItem(
+            icon: LucideIcons.external_link,
+            text: 'About Dart',
+            onPressed: () async {
+              final url = Uri.parse('https://dart.dev/');
+              if (await canLaunchUrl(url)) {
+                await launchUrl(url);
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}

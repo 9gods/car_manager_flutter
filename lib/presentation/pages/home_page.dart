255e6262a9bae5667ab3d8f1d9c8859469a505b1
9gods <codingcomelli@gmail.com>
2025-03-03 23:25:14 -0300

    feat: implements bottom navbar & homepage stats widget

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:garage_manager_flutter/presentation/widgets/colors/app_colors.dart';
+import 'package:garage_manager_flutter/presentation/widgets/home_page_stats.dart';
+import 'package:garage_manager_flutter/presentation/widgets/primitives/bottom_navbar.dart';
 import 'package:garage_manager_flutter/presentation/widgets/primitives/floating_app_bar.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,7 +11,16 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const FloatingAppBar(isHome: true),
-      body: const Center(child: Text('Home Page')),
+      bottomNavigationBar: const BottomNavbar(selectedIndex: 0),
+      backgroundColor: AppColors.slate900,
+      body: const Column(
+        spacing: 16,
+        children: [
+          SizedBox(height: 8),
+          HomePageStats(brandsCreated: '10', carsCreated: '37'),
+          Text('//carrousels', style: TextStyle(color: AppColors.slate300)),
+        ],
+      ),
     );
   }
 }

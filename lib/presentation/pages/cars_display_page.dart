b881990fb170a3f49d1d4fa73d9564a1c3788306
9gods <codingcomelli@gmail.com>
2025-03-03 23:24:44 -0300

    feat: implements bottom navbar widget

--- lib/presentation/pages/cars_display_page.dart
+++ lib/presentation/pages/cars_display_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:garage_manager_flutter/presentation/widgets/primitives/bottom_navbar.dart';
 import 'package:garage_manager_flutter/presentation/widgets/primitives/floating_app_bar.dart';
 
 class CarsDisplayPage extends StatelessWidget {
@@ -8,6 +9,7 @@ class CarsDisplayPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const FloatingAppBar(isHome: false, title: '• Cars display'),
+      bottomNavigationBar: const BottomNavbar(selectedIndex: 1),
       body: const Center(child: Text('Cars cards')),
     );
   }

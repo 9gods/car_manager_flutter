931771f3a1d5bf669c86c6f28574de3d73da46d3
9gods <codingcomelli@gmail.com>
2025-03-03 23:27:32 -0300

    refactor: change images paths, improve text styles

--- lib/presentation/widgets/primitives/custom_logo.dart
+++ lib/presentation/widgets/primitives/custom_logo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:garage_manager_flutter/presentation/widgets/colors/app_colors.dart';
 
 class CustomLogo extends StatelessWidget {
   final bool isHome;
@@ -10,16 +11,22 @@ class CustomLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (isHome) {
-      return Image.asset('assets/logo_normal.png', height: 40);
+      return Image.asset('/Logo.png');
     } else {
       return Row(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/logo_narrow.png', height: 40),
-          const SizedBox(width: 8),
-          Text(
-            title,
-            style: const TextStyle(fontSize: 20, color: Colors.black),
+          Image.asset('/LogoNarrow.png'),
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+                color: AppColors.slate300,
+              ),
+            ),
           ),
         ],
       );
